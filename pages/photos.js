373b8b973fce059c8530a91030f233f6145b31f1
rkekqmf <rkekqmf@gmail.com>
2022-06-20 13:52:28 +0900

    Add : styled components

---
+++ pages/photos.js
@@ -0,0 +1,12 @@
+import HeadInfo from "../components/HeadInfo";
+
+const photos = () => {
+  return (
+    <div>
+      <HeadInfo title="뽀토" />
+      <h1>My Photos</h1>
+    </div>
+  );
+};
+
+export default photos;
